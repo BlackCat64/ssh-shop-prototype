343c907e43060e83a7d71c8099febc87e96a1873
Sebastian Adegbite <Sebastianadegbite22@gmail.com>
2024-11-19 17:22:37 +0000

    Added basic page handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,77 @@
 import React from 'react';
 import './App.css';
-
+import { BrowserRouter as Router,
+  Route,
+  Routes,
+  Link} from "react-router-dom";
+//Basically main, used only to route pages
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
-    </div>
+      <Router>
+        <div>
+          <ul>
+          </ul>
+          <Routes>
+            <Route path="/" element={<Dashboard/>}/>
+            <Route path="/shop" element={<ShopHome/>}/>
+            {/*Nested routes, basically everything stored in ShopHome*/}
+            <Route path="results" element={<SearchResults/>}/>
+            <Route path="basket" element={<Basket />}/>
+            <Route path="checkout" element={<Checkout />}/>
+          </Routes>
+        </div>
+
+      </Router>
   );
 }
 
+//work on pages in their respective functions
+
+//Dashboard function is like homepage, just use to have button to Shop
+const Dashboard = () => (
+    <div>
+      <li>
+        <Link to="/shop">ShopHome</Link>
+      </li>
+    </div>
+)
+
+//Main page for us, will be adding items search bar etc
+const ShopHome = () => (
+    <div>
+      <h2>Shop Home</h2>
+      <ul>
+        <li>
+          <Link to="results">SearchResults</Link>
+        </li>
+        <li>
+          <Link to="basket">Basket</Link>
+        </li>
+        <li>
+          <Link to="checkout">Checkout</Link>
+        </li>
+      </ul>
+    </div>
+)
+
+//variable page that changes based on searches
+const SearchResults = () => (
+    <div>
+
+    </div>
+)
+
+//just displays items added to basket, and price(?)
+const Basket = () => (
+    <div>
+
+    </div>
+)
+
+//pretend to get user information and display price
+const Checkout = () => (
+    <div>
+
+    </div>
+)
 export default App;
