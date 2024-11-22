35738ae1cce8aeae605dec863d96b66b8f9a5374
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2024-11-22 17:27:13 +0000

    Filters now appear in a dropdown box.

--- src/Components/FilterSelect.tsx
+++ src/Components/FilterSelect.tsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PriceRangeSelector from './PriceRangeSelector'
 import SortSelector from './SortSelector'
 
-function FilterSelect() {
+const FilterSelect: React.FC = () => {
+    const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
+
+    const toggleDropdown = () => {
+        setDropdownOpen(!dropdownOpen);
+    };
+
+    // logic for when user clicks "Search" after setting filters goes here
+    const handleSearchClick = () => {
+        setDropdownOpen(false);
+    };
+
     return (
-        <div className="filterSelect">
-            <div className="title">
-                <label>Filters</label>
-            </div>
-            <br></br>
-            <PriceRangeSelector/>
-            <SortSelector/>
+        <div className="dropdown">
+            <button type="button" className="filtersDropdown btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded={dropdownOpen ? "true" : "false"}
+                    onClick={toggleDropdown}>
+                Filters
+            </button>
+            <form className="filtersMenu dropdown-menu p-4" aria-labelledby="filtersDropdown">
+                <div className="filterSelect">
+                    <table className="filterOptionsLayout">
+                        <th>
+                            <PriceRangeSelector/>
+                        </th>
+                        <th>
+                            <SortSelector/>
+                        </th>
+                    </table>
+                    <button className="btn btn-primary" type="button" onClick={handleSearchClick}>Search</button>
+                </div>
+            </form>
         </div>
     );
 }
--- src/Components/SortSelector.tsx
+++ src/Components/SortSelector.tsx
@@ -12,8 +12,7 @@ const SortSelect: React.FC = () => {
     const [sortType, setSortType] = useState<string>('alphabetical');
 
     const handleSortChange = (type: string) => {
-        setSortType(type); // Update the sort type
-        // You can add any logic here to handle the sorting of your data
+        setSortType(type);
         console.log("Selected Sort Type: ", type);
     };
 
@@ -25,7 +24,7 @@ const SortSelect: React.FC = () => {
 
             <div className="dropdown">
                 <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    {sortOptions[sortType]}  {/* Display the mapped sort option */}
+                    {sortOptions[sortType]}
                 </button>
                 <ul className="dropdown-menu">
                     {Object.keys(sortOptions).map((type) => (
@@ -34,7 +33,7 @@ const SortSelect: React.FC = () => {
                                 className="dropdown-item"
                                 onClick={() => handleSortChange(type)}
                             >
-                                {sortOptions[type]}  {/* Display the corresponding display name */}
+                                {sortOptions[type]}
                             </button>
                         </li>
                     ))}
