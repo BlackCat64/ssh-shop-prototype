e43f8adc50dff955b476d3e055c621ee66d88a5e
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2024-12-05 11:47:21 +0000

    Merged product view changes into this branch

--- src/App.tsx
+++ src/App.tsx
@@ -175,12 +175,30 @@ const SearchResults = () => {
     const queryParams = new URLSearchParams(location.search);
     const query = queryParams.get('query') || '';
 
-    // use this code for searching in the list of items, when we have it
-    // const results = mockData.filter(item =>
-    //     item.toLowerCase().includes(searchQuery.toLowerCase())
-    // );
+    const rawResults = productsDB.filter(item =>
+        item.name.toLowerCase().includes(query.toLowerCase())
+    );
+    // filter by price
+    const priceFilterResults = rawResults.filter(item =>
+        item.price >= minPrice && item.price <= maxPrice
+    );
+    // Sort the results based on sortType
+    const results = priceFilterResults.sort((a, b) => {
+        switch (sortType) {
+            case "alphabetical":
+                return a.name.localeCompare(b.name);
+            case "alphabetical-reverse":
+                return b.name.localeCompare(a.name);
+            case "price":
+                return a.price - b.price;
+            case "price-reverse":
+                return b.price - a.price;
+            default:
+                return 0;
+        }
+    });
 
-    return (<div className="searchResults">
+    return (<div className={"searchResults"}>
         <NavBar barName={"SSH Shop"} barNameLink="/shop"/>
         <FilterSelect
             sortType={sortType}
@@ -194,29 +212,61 @@ const SearchResults = () => {
             <p>Showing results for: <strong>{query}</strong></p>
             <p>Sorted by: <strong>{sortType}</strong></p>
             <p>Price Range: <strong>£{minPrice} - £{maxPrice}</strong></p>
-            <ProductDisplay
-                name={"Test"}
-                desc={"A very cool product"}
-                price={"£14.99"}
-                img={"https://media.discordapp.net/attachments/993444254447849523/1119610408504918026/853891953372168223.gif?ex=6747beb2&is=67466d32&hm=e9bdb151143b83d1d5822569cd28d89a4ee1286591c1d7fd38e65fc247b6f700&"}
-                id={"69"}
-            />
-            <ProductDisplay
-                name={"Test 2"}
-                desc={"Another very cool product"}
-                price={"£16.99"}
-                img={"https://cdn.discordapp.com/attachments/1246915842340556921/1311012156682272918/image.png?ex=6747f777&is=6746a5f7&hm=55f3ba8f0dd4383bb6ad03ede1d0df06dac09783e65ed606a37aa165d1b819c2&"}
-                id={"420"}
-            />
+            <div className="productList">
+                {results.map(product => ( // display search results
+                    <ProductDisplay
+                        name={product.name}
+                        summary={product.summary}
+                        price={displayPrice(product.price, "£")}
+                        img={product.images[0]}
+                        id={product.id}
+                    />
+                ))}
+            </div>
         </div>
     </div>);
 };
 
-const ViewProduct = () => (
-    <div>
+const ViewProduct = () => {
+    const location = useLocation();
+    const queryParams = new URLSearchParams(location.search);
+    const id: number = parseInt(queryParams.get('id') || '', 10);
+
+    if (isNaN(id))
+        return (
+            <div className="productView">
+                <NavBar barName={"SSH Shop"} barNameLink="/shop"/>
+                <p><strong>Invalid Product ID</strong></p>
+            </div>
+        );
+
+    const product = productsDB.find(p => p.id == id);
+    if (!product)
+        return (
+            <div className="productView">
+                <NavBar barName={"SSH Shop"} barNameLink="/shop"/>
+                <p>Product Not Found</p>
+            </div>
+        );
+
+
+    return (
+    <div className="productView">
         <NavBar barName={"SSH Shop"} barNameLink="/shop"/>
+        <ImageCarousel images={product.images}/>
+        <div className="productDetails">
+            <div className="productHeader">
+                <p className="productName">{product.name}</p>
+                <button className="btn btn-primary addToBasket">Add to Basket</button>
+            </div>
+
+            <p className="productPrice">{displayPrice(product.price, "£")}</p>
+            <p style={{fontWeight: 'bold'}}>Item Details:</p>
+            <p className="productDesc">{product.desc.split('\n').map(line => line.trim()).join('\n')}</p>
+        </div>
     </div>
-);
+    );
+};
 
 //just displays items added to basket, and price(?)
 const Basket = () => (
@@ -238,4 +288,10 @@ const Settings = () => (
         <NavBar barName={"SSH Shop"} barNameLink="/shop"/>
     </div>
 );
+
+
+function displayPrice(price: number, currency: string): string {
+    return currency + price.toFixed(2);
+}
+
 export default App;
